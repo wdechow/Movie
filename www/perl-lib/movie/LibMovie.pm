d873cf1d6044ff81a5c3f8f7bbdf6ae2c1d4ec14
Wolfgang Dechow <wolfgang.dechow@gmail.com>
2014-01-04 12:48:23 +0100

    Kommentare umgesetzt

--- www/perl-lib/movie/LibMovie.pm
+++ www/perl-lib/movie/LibMovie.pm
@@ -13,7 +13,7 @@ use DBD::mysql;
 # ==============
 # prepares and executes the given sql statement
 #
-# @param $local_dbh database handler
+# @param $db_handler database handler
 # @param $sql SQL statement to execute
 # @param *varargs* variables for the sql statement (these replace the ? in $sql)
 #
@@ -21,20 +21,20 @@ use DBD::mysql;
 #
 sub execute_sql
 {
-  my $local_dbh = shift;
-  my $sql       = shift;
+  my $db_handler  = shift;
+  my $sql         = shift;
 
   my $sqlh;
 
-  unless( $sqlh = $local_dbh -> prepare( $sql ) )
+  unless( $sqlh = $db_handler -> prepare( $sql ) )
   {
-    print "Can't prepare statement: $sql\n";
+    warn "Can't prepare statement: $sql\n";
     return;
   }
 
   unless( $sqlh -> execute( @_ ) )
   {
-    print "Can't execute statement: $sql, " . join( ", ", @_ ) . "\n";
+    warn "Can't execute statement: $sql, " . join( ", ", @_ ) . "\n";
     return;
   }
 
@@ -49,33 +49,40 @@ sub execute_sql
 #
 sub db_connect
 {
-  my $c = "DBI:mysql:database=MovieDB_1_0;host=127.0.0.1";
-  my $u = "movie";
+  my $db_connection_string  = "DBI:mysql:database=MovieDB_1_0;host=127.0.0.1";
+  my $user                  = "movie";
   # a secure password is pretty useless if stored in public :D
   # well useless information does not need protection
-  my $p = "P4l.xA#3W?s_";
-  my $d;
+  my $password              = "P4l.xA#3W?s_";
+  my $db_handler;
 
-  unless( $d = DBI -> connect( $c, $u, $p ) )
+  unless( $db_handler = DBI -> connect( $db_connection_string, $user, $password ) )
   {
-      return;
+    return;
   }
 
-  $d -> do( "set autocommit = 1" );
+  $db_handler -> do( "set autocommit = 1" );
 
-  return $d;
+  return $db_handler;
 }
 
 
 # ==============
 # closes the database connection
 #
-# @param $d database handler
+# @param $db_handler database handler
 #
 sub db_close
 {
-  my $d = shift;
-  $d -> disconnect( );
+  my $db_handler;
+
+  unless( $db_handler = shift )
+  {
+    warn "Can't close DB connection without database handler!\n";
+    return;
+  }
+
+  $db_handler -> disconnect( );
 }
 
 
