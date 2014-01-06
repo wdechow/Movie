57d846a68b8b9a65e0f3a3c724ab74ebacfbebb6
Wolfgang Dechow <wolfgang.dechow@gmail.com>
2014-01-06 15:19:33 +0100

    Erste Version des Menüs hinzugefügt

--- www/htdocs/movie/script/movie.js
+++ www/htdocs/movie/script/movie.js
@@ -12,9 +12,13 @@ $( document ).ready( function( ) {
  */
 function load_site( )
 {
-  $( "#movie-header" ).load( "movie/components/header.html" );
-  $( "#movie-content" ).load( "movie/components/main.html" );
-  $( "#movie-footer" ).load( "movie/components/footer.html" );
+  $( "#header" ).load( "movie/components/header.html", function( ) {
+    // When the header is fully loaded, i can set the onclick handlers for the links
+    load_menu( );
+  } );
+
+  $( "#content" ).load( "movie/components/main.html" );
+  $( "#footer" ).load( "movie/components/footer.html" );
 
   // Request to test mod_perl
   $.post( path, { }, function( data ) {
@@ -22,4 +26,54 @@ function load_site( )
   } ).success( function( ) {
 
   } );
+}
+
+
+/**
+ * This function sets the onclock handler for all menu entries
+ */
+function load_menu( )
+{
+  var link_preferences    = document.getElementById( "menu-preferences" );
+  var link_detail_search  = document.getElementById( "menu-detail-search" );
+  var link_add            = document.getElementById( "menu-add" );
+
+  link_preferences.onclick = function( ) {
+    handler_menu_preferences( );
+  }
+
+  link_detail_search.onclick = function( ) {
+    handler_menu_detail_search( );
+  }
+
+  link_add.onclick = function( ) {
+    handler_menu_add( );
+  }
+}
+
+
+/**
+ * This function handles the onclick event of the preferences menu entry
+ */
+function handler_menu_preferences( )
+{
+  console.log( "handler_menu_preferences" );
+}
+
+
+/**
+ * This function handles the onclick event of the detail search menu entry
+ */
+function handler_menu_detail_search( )
+{
+  console.log( "handler_menu_detail_search" );
+}
+
+
+/**
+ * This function handles the onclick event of the add menu entry
+ */
+function handler_menu_add( )
+{
+  console.log( "handler_menu_add" );
 }
