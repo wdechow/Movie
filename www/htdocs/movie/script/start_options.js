ff30f40da5cbd206151ca1f9c2a2ee5d46517ecc
Wolfgang Dechow <wolfgang.dechow@gmail.com>
2014-01-10 22:59:26 +0100

    Titel- und Filterleiste von table getrennt, ...
    Tabellenlinien angepasst, Hintergrund der Inputfelder angepasst, Spacer trennt Tabellen, Start- und Detail- Div eingefügt

---
+++ www/htdocs/movie/script/start_options.js
@@ -0,0 +1,17 @@
+
+$( document ).ready( function( ) {
+  set_start_option_handler( );
+  load_details( );
+} );
+
+
+function set_start_option_handler( )
+{
+  console.log( "set_start_option_handler" );
+}
+
+
+function load_details( )
+{
+  console.log( "load_details" );
+}
